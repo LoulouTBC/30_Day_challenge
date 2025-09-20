1faf97dd7e74b37160ad665d53fd91eab09098b1
loulouTBC <loulouibrahim21@gmail.com>
2025-09-20 21:53:23 +0300

    progress screen/enable dynamic range selection

--- lib/features/progress_calendar/ui/progress_calendar_screen.dart
+++ lib/features/progress_calendar/ui/progress_calendar_screen.dart
@@ -12,8 +12,8 @@ class _GlobalCalendarState extends State<GlobalCalendar> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
-  DateTime? _rangeStart;
-  DateTime? _rangeEnd;
+  DateTime? _rangeStart = DateTime.utc(2025, 9, 2);
+  DateTime? _rangeEnd = DateTime.utc(2025, 9, 10);
 
   var selectedDayes = [];
 
@@ -25,6 +25,7 @@ class _GlobalCalendarState extends State<GlobalCalendar> {
         children: [
           SizedBox(height: 200),
           TableCalendar(
+            // rangeSelectionMode: RangeSelectionMode.toggledOn,
             firstDay: DateTime.utc(2010, 10, 16),
             lastDay: DateTime.utc(2030, 3, 14),
             focusedDay: _focusedDay,
@@ -32,21 +33,20 @@ class _GlobalCalendarState extends State<GlobalCalendar> {
             rangeStartDay: _rangeStart,
             rangeEndDay: _rangeEnd,
             onRangeSelected: (start, end, focusedDay) => {
-              DateTime.utc(2025, 9, 5),
-              DateTime.utc(2025, 9, 5),
               setState(() {
-  _rangeStart = start;
-  _rangeEnd = end;
-});
+                _rangeStart = start ?? _rangeStart;
+                _rangeEnd = end ?? _rangeEnd;
+                _focusedDay = focusedDay;
+              }),
             },
-            selectedDayPredicate: (day) {
-              // Use `selectedDayPredicate` to determine which day is currently selected.
-              // If this returns true, then `day` will be marked as selected.
+            // selectedDayPredicate: (day) {
+            //   // Use `selectedDayPredicate` to determine which day is currently selected.
+            //   // If this returns true, then `day` will be marked as selected.
 
-              // Using `isSameDay` is recommended to disregard
-              // the time-part of compared DateTime objects.
-              return isSameDay(_selectedDay, day);
-            },
+            //   // Using `isSameDay` is recommended to disregard
+            //   // the time-part of compared DateTime objects.
+            //   return isSameDay(_selectedDay, day);
+            // },
             onDaySelected: (selectedDay, focusedDay) {
               if (!isSameDay(_selectedDay, selectedDay)) {
                 // Call `setState()` when updating the selected day
@@ -55,11 +55,41 @@ class _GlobalCalendarState extends State<GlobalCalendar> {
                   _focusedDay = focusedDay;
                 });
               }
-                if (_rangeStart != null && _rangeEnd != null) {
-    return day.isAfter(_rangeStart!.subtract(const Duration(days: 1))) &&
-           day.isBefore(_rangeEnd!.add(const Duration(days: 1)));
-  }
-            
+               setState(() {
+                // If no range exists, start a new range
+                if (_rangeStart == null) {
+                  _rangeStart = selectedDay;
+                  _rangeEnd = selectedDay;
+                } else {
+                  // If selected day is before the current start, update start
+                  if (selectedDay.isBefore(_rangeStart!)) {
+                    _rangeStart = selectedDay;
+                  } else {
+                    // If selected day is after current end, extend the range
+                    if (_rangeEnd == null || selectedDay.isAfter(_rangeEnd!)) {
+                      _rangeEnd = selectedDay;
+                    }
+                  }
+                }
+
+                // Update focused day to selected day
+                _focusedDay = focusedDay;
+              });
+              // setState(() {
+              //   if (_rangeStart == null || _rangeEnd != null) {
+              //     // new Range 
+              //     _rangeStart = selectedDay;
+              //     _rangeEnd = null;
+              //   } else {
+              
+              //     if (selectedDay.isBefore(_rangeStart!)) {
+              //       _rangeStart = selectedDay; 
+              //     } else {
+              //       _rangeEnd = selectedDay; 
+              //     }
+              //   }
+              //   _focusedDay = focusedDay;
+              // });
             },
             onFormatChanged: (format) {
               if (_calendarFormat != format) {
